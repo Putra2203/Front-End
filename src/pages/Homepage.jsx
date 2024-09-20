275ffe498dbacb9ceb4ebe2fa7cca222ffacd5ba
Putra2203 <erdin22xii@gmail.com>
2024-09-20 21:27:39 +0700

    update home admin

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,26 +1,54 @@
-import React, { useState, useEffect } from "react"
-import axios from 'axios'
-import jwt_decode from "jwt-decode"
-import { useNavigate, useLocation } from 'react-router-dom'
-import imageCon from "../Assets/balaikota.png"
-import logo from "../Assets/diskominfo.png"
-import icon from "../Assets/icon.png"
-import penugasan from "../Assets/image_Buat Penugasan.svg"
-import peserta from "../Assets/image_Peserta magang.svg"
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap-icons/font/bootstrap-icons.css"
-import "../Components/SideBar/Navbar.css"
-import './Homestyle.css'
-import { TabTitle } from "../TabName"
+import React, { useState, useEffect } from "react";
+import { axiosJWTadmin } from "../config/axiosJWT";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate, useLocation } from "react-router-dom";
+import logo from "../Assets/diskominfo.png";
 import Footer from "./footer";
+import "../Components/SideBar/Navbar.css";
+import "../Components/SideBar/Sidebar.css";
+import "./Homestyle.css";
+import { TabTitle } from "../TabName";
 
 const Homepage = () => {
-  TabTitle('Homepage');
-  const [nama, setNama] = useState('');
+  TabTitle("Homepage");
+  const [nama, setNama] = useState("");
+  const [fotoProfil, setFotoProfil] = useState("");
   const navigate = useNavigate();
   const [showNav, setShowNav] = useState(false);
   const location = useLocation();
   const [activeLink, setActiveLink] = useState(location.pathname);
+
+  //jumlah total attendance
+  const [totalAttendance, setTotalAttendance] = useState(0);
+  const [searchDate, setSearchDate] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getUsers();
+    const today = new Date().toISOString().slice(0, 10);
+    setSearchDate(today);
+  }, []);
+
+  useEffect(() => {
+    getUsers();
+  }, [searchDate]);
+
+  const getUsers = async () => {
+    setLoading(true);
+    const url = searchDate
+      ? `http://localhost:3000/admin/presensi?tanggal=${searchDate}`
+      : "http://localhost:3000/admin/presensi";
+
+    try {
+      const response = await axiosJWTadmin.get(url);
+      setTotalAttendance(response.data.totalSudahPresensi);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleNavLinkClick = (path) => {
     setActiveLink(path);
@@ -29,143 +57,243 @@ const Homepage = () => {
   useEffect(() => {
     refreshToken();
     setActiveLink(location.pathname);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.pathname]);
 
   const refreshToken = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/account/token', {
+      const response = await axios.get("http://localhost:3000/account/token", {
         headers: {
-          'role': "admin"
+          role: "admin",
         },
       });
       const decoded = jwt_decode(response.data.token);
       setNama(decoded.nama);
+      setFotoProfil(decoded.foto_profil); // Menyimpan foto profil ke state
     } catch (error) {
       if (error.response) {
         navigate("/");
       }
     }
-  }
-
+  };
 
   return (
     <div className="body-main">
       <div className={`body-area${showNav ? " body-pd" : ""}`}>
         <header className={`header${showNav ? " body-pd" : ""}`}>
-          <div className="header_toggle">
-          </div>
-          <div className="header_img">
-            <img
-              src={icon}
-              alt=""
-            />
-          </div>
+          <div className="header_toggle"></div>
+          <div className="header_img"></div>
         </header>
         <div className={`sidebar${showNav ? " open" : ""}`}>
           <div className="logo-details">
-            <i className='bx bxl-c-plus-plus icon'></i>
-            <a
-              href="/homepage"
-              target="_self"
-              className="logo_name"
-            >
+            <i className="bx bxl-c-plus-plus icon"></i>
+            <a href="/homepage" target="_self" className="logo_name">
               <img
                 src={logo}
                 alt=""
                 style={{ width: "120px", height: "auto" }}
               />
             </a>
-            <i class='bi-list' id="btn" onClick={() => setShowNav(!showNav)}></i>
+            <i
+              className="bi-list"
+              id="btn"
+              onClick={() => setShowNav(!showNav)}
+            ></i>
           </div>
-          <ul class="nav-list">
+          <ul className="nav-list">
+            <div>
+              <img src={fotoProfil} alt="" />
+              <h1>CEK {nama}</h1>
+            </div>
             <li>
-              <a href="homepage">
+              <a
+                href="homepage"
+                onClick={() => handleNavLinkClick("homepage")}
+                className={activeLink === "homepage" ? " active" : ""}
+              >
                 <i className="bi bi-house nav_icon" />
-                <span class="links_name">Home</span>
+                <span className="links_name">Home</span>
               </a>
-              <span class="tooltip">Home</span>
+              <span className="tooltip">Home</span>
             </li>
             <li>
-              <a href="admin">
+              <a
+                href="admin"
+                onClick={() => handleNavLinkClick("admin")}
+                className={activeLink === "admin" ? " active" : ""}
+              >
                 <i className="bi bi-person-check-fill nav_icon" />
-                <span class="links_name">Admin</span>
+                <span className="links_name">Admin</span>
               </a>
-              <span class="tooltip">Admin</span>
+              <span className="tooltip">Admin</span>
             </li>
             <li>
-              <a href="peserta">
+              <a
+                href="peserta"
+                onClick={() => handleNavLinkClick("peserta")}
+                className={activeLink === "peserta" ? " active" : ""}
+              >
                 <i className="bi bi-person nav_icon" />
-                <span class="links_name">Peserta</span>
+                <span className="links_name">Peserta</span>
               </a>
-              <span class="tooltip">Peserta</span>
+              <span className="tooltip">Peserta</span>
             </li>
             <li>
-              <a href="presensi">
+              <a
+                href="presensi"
+                onClick={() => handleNavLinkClick("presensi")}
+                className={activeLink === "presensi" ? " active" : ""}
+              >
                 <i className="bi bi-person-check nav_icon" />
-                <span class="links_name">Presensi Magang</span>
+                <span className="links_name">Presensi Magang</span>
               </a>
-              <span class="tooltip">Presensi Magang</span>
+              <span className="tooltip">Presensi Magang</span>
             </li>
             <li>
-              <a href="penugasan">
+              <a
+                href="penugasan"
+                onClick={() => handleNavLinkClick("penugasan")}
+                className={activeLink === "penugasan" ? " active" : ""}
+              >
                 <i className="bi bi-list-task nav_icon" />
-                <span class="links_name">Penugasan</span>
+                <span className="links_name">Penugasan</span>
               </a>
-              <span class="tooltip">Penugasan</span>
+              <span className="tooltip">Penugasan</span>
             </li>
             <li>
-              <a href="profile">
+              <a
+                href="profile"
+                onClick={() => handleNavLinkClick("profile")}
+                className={activeLink === "profile" ? " active" : ""}
+              >
                 <i className="bi bi-person nav_icon" />
-                <span class="links_name">Profile</span>
+                <span className="links_name">Profile</span>
               </a>
-              <span class="tooltip">Profile</span>
+              <span className="tooltip">Profile</span>
             </li>
             <li className="profile">
-
-              <a href="/">
-
-                <i className="bi bi-box-arrow-left nav_icon" ></i>
-                <span class="links_name">Sign Out</span>
+              <a
+                href="/"
+                onClick={() => handleNavLinkClick("/")}
+                className="sign-out"
+              >
+                <i className="bi bi-box-arrow-left nav_icon"></i>
+                <span className="links_name">Sign Out</span>
               </a>
             </li>
           </ul>
         </div>
+
         <div className="home-section">
-          <div className="homepage">
-            <div className="account-info-container">
-              <div className="info-box">
-                <div className="user-info">
-                  <p className="user-info-1">Selamat Datang,</p>
-                  <p className="user-info-1">{nama}</p>
-                </div>
+          <div className="homepage-container">
+            <h1 className="poppins-semibold">Dashboard - SISAPPMA</h1>
+            <div className="userdata-1">
+              <div className="userdata-wrapper">
+                <h3>Jumlah Mahasiswa Absen Hari ini</h3>
+                <p>{totalAttendance}</p>
               </div>
-              <div className='space'></div>
-              <div className="user-image">
-                <img src={icon} alt="" />
+              <div className="userdata-wrapper">
+                <h3>Jumlah Mahasiswa Absen Hari ini</h3>
+                <p>//jumlah</p>
               </div>
-            </div>
-            <div className="homepage"></div>
-            <div className="image-container">
-              <img className="background-home" src={imageCon} alt='' />
+              <div className="userdata-wrapper">
+                <h3>Jumlah Mahasiswa Absen Hari ini</h3>
+                <p>//jumlah</p>
+              </div>
             </div>
 
-          </div>
-          <div className="action-buttons">
-            <a href="/peserta">
-              <img src={peserta} alt="" />
-              <span>Peserta</span>
-            </a>
-            <a href="/penugasan">
-              <img src={penugasan} alt="" />
-              <span>Penugasan</span>
-            </a>
+            <h1 className="poppins-semibold">Data Penugasan</h1>
+            <div className="table-contaiener">
+              <table className="styled-table">
+                <thead>
+                  <tr>
+                    <th>Ketua Kelompok</th>
+                    <th>Judul</th>
+                    <th>Deskripsi</th>
+                    <th>Dateline</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Erdin Purwa Putra</td>
+                    <td>SISAPPMA</td>
+                    <td>
+                      Nemo excepturi repellat voluptatum cum, quibusdam, culpa
+                      perferendis sit iure inventore modi ea incidunt voluptas
+                      deserunt
+                    </td>
+                    <td>0209209</td>
+                  </tr>
+                  <tr>
+                    <td>Putra</td>
+                    <td>SISAPPMA</td>
+                    <td>
+                      perferendis sit iure inventore modi ea incidunt voluptas
+                      deserunt voluptatibus autem placeat nam, totam adipisci.
+                    </td>
+                    <td>0209209</td>
+                  </tr>
+                  <tr>
+                    <td>Putra</td>
+                    <td>SISAPPMA</td>
+                    <td>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nemo excepturi
+                    </td>
+                    <td>0209209</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <h1 className="poppins-semibold">Data Absensi</h1>
+            <div className="table-contaiener">
+              <table className="styled-table">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Instansi</th>
+                    <th>Persentase Kehadiran</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Putra</td>
+                    <td>SISAPPMA</td>
+                    <td>
+                      Nemo excepturi repellat voluptatum cum, quibusdam, culpa
+                      perferendis sit iure inventore modi ea incidunt voluptas
+                      deserunt
+                    </td>
+                    <td>0209209</td>
+                  </tr>
+                  <tr>
+                    <td>Putra</td>
+                    <td>SISAPPMA</td>
+                    <td>
+                      perferendis sit iure inventore modi ea incidunt voluptas
+                      deserunt voluptatibus autem placeat nam, totam adipisci.
+                    </td>
+                    <td>0209209</td>
+                  </tr>
+                  <tr>
+                    <td>Putra</td>
+                    <td>SISAPPMA</td>
+                    <td>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nemo excepturi
+                    </td>
+                    <td>0209209</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-      <Footer /> 
+      <Footer />
     </div>
   );
-}
+};
 
 export default Homepage;
--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import './footer.css';
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p>SISAPPNA</p>
-        <p>&copy; 2024 Semua Hak Dilindungi.</p>
-        <p>
-          <a href="https://diskominfo.semarangkota.go.id/" target="_blank" rel="noopener noreferrer">Website</a> |
-          <a href="https://instagram.com/diskominfokotasemarang" target="_blank" rel="noopener noreferrer">Instagram</a>
-        </p>
+      <div className="footer-content">      
+        <p>SISAPPMA</p>
+        <p>2024 &copy; all right reserved.</p>
       </div>
     </footer>
   );
